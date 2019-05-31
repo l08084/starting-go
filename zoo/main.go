c8362cf9717c65110665ae0cf0784b7729c07ac9
l08084 <l08084.1989@gmail.com>
2019-05-31 21:16:14 +0900

    update

--- zoo/main.go
+++ zoo/main.go
@@ -7,6 +7,8 @@ import (
 )
 
 func main() {
+	fmt.Println(AppName()) // 関数AppNameの呼び出しを追加
+	
 	fmt.Println(animals.ElephantFeed())
 	fmt.Println(animals.MonkeyFeed())
 	fmt.Println(animals.RabbitFeed())
